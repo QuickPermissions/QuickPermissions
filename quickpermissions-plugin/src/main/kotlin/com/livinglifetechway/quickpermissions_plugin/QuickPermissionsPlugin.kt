3776b669abaee1f83924a113dfff47d0092840dd
Kirtan Thakkar <kirtan403@gmail.com>
2018-02-13 19:04:35 +0530

    Update quickpermissions-annotations dependency

--- quickpermissions-plugin/src/main/kotlin/com/livinglifetechway/quickpermissions_plugin/QuickPermissionsPlugin.kt
+++ quickpermissions-plugin/src/main/kotlin/com/livinglifetechway/quickpermissions_plugin/QuickPermissionsPlugin.kt
@@ -26,7 +26,7 @@ class QuickPermissionsPlugin : Plugin<Project> {
 
                 // add necessary dependencies
                 project.dependencies.add("implementation", "org.aspectj:aspectjrt:1.8.13")
-                project.dependencies.add("api", "com.github.quickpermissions:quickpermissions:27af3f4d9e")
+                project.dependencies.add("api", "com.github.quickpermissions:quickpermissions-annotations:27af3f4d9e")
 
             } else {
                 // throw exception it doesn't work on any other module
