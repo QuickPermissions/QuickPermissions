3bbbcd075446bcc76ebf3e3d43a0a9f0e5e2d4e6
Kirtan Thakkar <kirtan403@gmail.com>
2018-02-28 16:49:50 +0530

    Add quickpermissions only and detailed logs

--- quickpermissions-plugin/src/main/kotlin/com/livinglifetechway/quickpermissions_plugin/TempTransform.kt
+++ quickpermissions-plugin/src/main/kotlin/com/livinglifetechway/quickpermissions_plugin/TempTransform.kt
@@ -33,8 +33,9 @@ class TempTransform(private val project: Project) : Transform() {
             }
 
             it.jarInputs.forEach {
-                if (it.file.name.contains("quickpermissions-annotations") || it.file.name.contains("aspectjrt")) {
-                    log("REF JAR INPUT" + it.file.toString())
+                log("REF JAR INPUT" + it.file.toString())
+                if (it.file.name.contains("quickpermissions") || it.file.name.contains("aspectjrt")) {
+                    log("REF JAR [ADDED]")
                     classpathFiles.add(it.file)
                 }
             }
